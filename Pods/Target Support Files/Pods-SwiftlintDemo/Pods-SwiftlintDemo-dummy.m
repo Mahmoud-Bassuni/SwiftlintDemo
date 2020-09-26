66aeaad93480796ea85285f6f17a85659683859b
mahmoud bassuni <mahmoudbasuni90@gmail.com>
2020-09-26 14:11:56 +0200

    Initial commit

---
+++ Pods/Target Support Files/Pods-SwiftlintDemo/Pods-SwiftlintDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftlintDemo : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftlintDemo
+@end
---
+++ SwiftlintDemo/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SwiftlintDemo
+//
+//  Created by Bassuni on 9/24/20.
+//  Copyright © 2020 Mahmoud Bassuni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
